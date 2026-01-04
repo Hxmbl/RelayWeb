891e1237d71728d7a86b38f7c488e8ef19e879b6
Simi <180758703+Hxmbl@users.noreply.github.com>
2026-01-04 23:57:46 +0000

    base created

---
+++ frontend/main.tsx
@@ -0,0 +1,6 @@
+/** @jsxImportSource solid-js */
+import { render } from "solid-js/web";
+
+const App = () => <h1 class="text-3xl font-bold text-blue-600">Welcome to RelayWeb!</h1>;
+
+render(App, document.getElementById("app")!);
