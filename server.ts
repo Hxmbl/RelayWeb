d320d0f004ac92b5146aa9538953ebafa3625e8e
Simi <180758703+Hxmbl@users.noreply.github.com>
2026-01-09 23:35:24 +0000

    we're restarting idc

--- server.ts
+++
@@ -1,7 +0,0 @@
-import { serve } from "https://deno.land/std@0.205.0/http/server.ts";
-
-console.log("RelayWeb running on http://localhost:8000");
-
-serve((_req) => {
-    return new Response("Hello from RelayWeb!", { status: 200 });
-});
